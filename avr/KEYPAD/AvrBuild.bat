e66fce4f95f1afe30a8b592f8fe099448143ae69
Saber Sojudi <108491532+sabers13@users.noreply.github.com>
2025-11-16 07:46:05 +0100

    Delete avr/KEYPAD directory

--- avr/KEYPAD/AvrBuild.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-del "d:\micro\keypad\keypad.map"
-del "d:\micro\keypad\labels.tmp"
-"C:\Program Files (x86)\Atmel\AVR Tools\AvrAssembler2\avrasm2.exe" -S "d:\micro\keypad\labels.tmp" -fI  -o "d:\micro\keypad\keypad.hex" -d "d:\micro\keypad\keypad.obj" -e "d:\micro\keypad\keypad.eep" -m "d:\micro\keypad\keypad.map" -W+ie   "D:\micro\KEYPAD\KEYPAD.asm"
